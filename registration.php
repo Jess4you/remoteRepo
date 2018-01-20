f618c6b9a79631ae884ba17a6b41c5a2e308fe65
John Gabriel Singson <jgsingson@gmail.com>
2018-01-20 21:57:33 +0800

    Modified Registration.php

--- registration.php
+++ registration.php
@@ -1,3 +1,15 @@
+<?
+<?php
+$con = mysqli_connect("localhost","root","","database");
+
+// Check connection
+if (mysqli_connect_errno())
+  {
+  echo "Failed to connect to MySQL: " . mysqli_connect_error();
+  }
+?>
+
+?>
 <html>
 
 <head>
